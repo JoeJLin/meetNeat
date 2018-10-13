7781d73c2d8c2e3c1ac2e98ab0bb12f8166448a8
David Chiu <chchiu1028@gmail.com>
2018-10-12 23:46:52 -0400

    mergefix

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import AppNavBar from "./component/AppNavBar"
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <AppNavBar/>
         hello
       </div>
     );
