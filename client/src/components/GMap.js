aa976d548189b2ff2c1e1f6ea2e20679a1674fa5
David Chiu <chchiu1028@gmail.com>
2018-10-13 06:42:01 -0400

    gmaps added

---
+++ client/src/components/GMap.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
+
+const AnyReactComponent = ({text}) => <div>{text}</div>
+
+class GMap extends Component {
+  static defaultProps = {
+    center: {lat:40.728283, lng:-73.990},
+    zoom: 11
+  };
+
+  render() {
+    return (
+      <div class="container" style={{height: '50vh', width: '100%'}}>
+        <GoogleMapReact
+          bootstrapURLkeys={{key:process.env.REACT_APP_GOOGLE_MAP}}
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom} >
+          <AnyReactComponent
+            lat={40.728283}
+            lng={-73.990339}
+            text={'test'}/>
+        </GoogleMapReact>
+      </div>
+    );
+  }
+}
+
+export default GMap;
