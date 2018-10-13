301498e72e5e7aada1b3a50c15df0c02fd162c9b
JoeJLin <Joe.j.lin21@gmail.com>
2018-10-12 23:45:17 -0400

    merge joe

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 import Facebook from './components/Facebook';
+import AppNavBar from './components/AppNavBar';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <AppNavBar />
         <Facebook />
       </div>
     );
